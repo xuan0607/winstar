56e756d49febcd6c43b5d42fbd5c4b7e0edc2a69
xuan0607 <133654414+xuan0607@users.noreply.github.com>
2024-04-12 21:57:37 +0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,130 @@
+M: {
+	M1: {
+		description: 'Austenitic Stainless Steel',
+		HB_Hardness: '130 ~ 200',
+		HRC_Hardness: '-',
+		standard_Din: ['X5CrNi1810', 'GX6CrNiMo1810', 'X6CrNiMoTi17122', 'X8CrNiMoAl1572'],
+	},
+	M2: {
+		description: 'High-Strength Austenitic Stainless, Cast Stainless',
+		HB_Hardness: '150 ~ 230',
+		HRC_Hardness: '< 25',
+		standard_Din: ['X2CrNiMo134', 'X10CrNi189', 'X5CrNiCuNb1744'],
+	},
+	M3: {
+		description: 'Duplex Stainless Steel',
+		HB_Hardness: '135 ~ 275',
+		HRC_Hardness: '< 30',
+		standard_Din: ['X2CrNiMoN2315', 'X20CrNiSi254', 'GX40CrNiSi274', 'X2CrNiMoN2253'],
+	}
+}
+K: {
+	K1: {
+		description: 'Grey Cast Iron',
+		HB_Hardness: '120 ~ 290',
+		HRC_Hardness: '< 32',
+		standard_Din: ['GG10', 'GG15', 'GG20', 'GG25', 'GG40'],
+	},
+	K2: {
+		description: 'Low- and Medium-Strength Ductile Irons and Compacted Graphite Irons (CGI)',
+		HB_Hardness: '130 ~ 260',
+		HRC_Hardness: '< 28',
+		standard_Din: ['GGG35.3', 'GGG40.3', 'GGG70'],
+	},
+	K3: {
+		description: 'High-Strength Ductile Irons and Austempered Ductile Iron (ADI)',
+		HB_Hardness: '180 ~ 350',
+		HRC_Hardness: '< 43',
+		standard_Din: ['GTW-35-04', 'GTW-65', 'GTS-35-10', 'GTS-45-06'],
+	}
+}
+N: {
+	N1: {
+		description: 'Wrought Aluminium',
+		HB_Hardness: '60 ~ 90',
+		HRC_Hardness: '-',
+		standard_Din: ['AIMg1', 'AI99.5', 'AICuMg1', 'AICuBiPb', 'AIMgSi1', 'ALMgSiPb'],
+	},
+	N2: {
+		description: 'Low-Silicon Aluminium Alloys',
+		HB_Hardness: '70 ~ 120',
+		HRC_Hardness: '-',
+		standard_Din: ['GAISiCu4', 'GDAISi10Mg'],
+	},
+	N3: {
+		description: 'High-Silicon Aluminium Alloys',
+		HB_Hardness: '60 ~ 120',
+		HRC_Hardness: '-',
+		standard_Din: ['G-ALSi12', 'G-AlSi17Cu4', 'G-AISi21CuNiMg'],
+	}
+    N4: {
+		description: 'Copper and Copper Alloys',
+		HB_Hardness: '60 ~ 200',
+		HRC_Hardness: '-',
+		standard_Din: ['CuZn40', 'Ms60', 'G-CuSn5ZnPb', 'CuZn37', 'CuSi3Mn'],
+	}
+    N5: {
+		description: 'Nylon, Plastics',
+		HB_Hardness: '-',
+		HRC_Hardness: '-',
+		standard_Din: ['Lexan', 'Hostalen', 'Polystyrol', 'Makrolon'],
+	}
+    N6: {
+		description: 'Carbon, Graphite Composites, CFRP',
+		HB_Hardness: '-',
+		HRC_Hardness: '-',
+		standard_Din: ['CFRP', 'CFK', 'GFK'],
+	}
+}
+S: {
+	S1: {
+		description: 'Iron-Based, Heat-Resistant Alloys',
+		HB_Hardness: '160 ~ 260',
+		HRC_Hardness: '25 ~ 48',
+		standard_Din: ['X1NiCrMoCu32287', 'X12NiCrSi3616', 'X5NiCrAITi3120', 'X40CoCrNi20 20'],
+	},
+	S2: {
+		description: 'Cobalt-Based, Heat-Resistant Alloys',
+		HB_Hardness: '250 ~ 450',
+		HRC_Hardness: '25 ~ 48',
+		standard_Din: ['Haynes 188', 'Stellite 6', '21', '31'],
+	},
+	S3: {
+		description: 'Nickel-Based, Heat-Resistant Alloys',
+		HB_Hardness: '160 ~ 450',
+		HRC_Hardness: '33 ~ 48',
+		standard_Din: ['NiCr29Fe', 'NiCr22Mo9Nb', 'NiCr20Mo', 'NiCr19FeNbMo', 'NiCr16Fe7TiAl'],
+	}
+    S4: {
+		description: 'Titanium and Titanium Alloys',
+		HB_Hardness: '300 ~ 400',
+		HRC_Hardness: '33 ~ 48',
+		standard_Din: ['Ti1', 'TiAI5Sn2', 'TiAI6V4', 'TiAI4Mo4Sn2'],
+	}
+}
+H: {
+	H1: {
+		description: 'Hardened Materials',
+		HB_Hardness: '< 460',
+		HRC_Hardness: '< 48',
+		standard_Din: ['GX260NiCr42', 'GX330NiCr42', 'GX300CrNiSi952', 'GX300CrMo153'],
+	},
+	H2: {
+		description: 'Hardened Materials',
+		HB_Hardness: '460 ~ 560',
+		HRC_Hardness: '48 ~ 55',
+		standard_Din: ['-'],
+	},
+	H3: {
+		description: 'Hardened Materials',
+		HB_Hardness: '560 ~ 650',
+		HRC_Hardness: '56 ~ 60',
+		standard_Din: ['-'],
+	}
+    H4: {
+		description: 'Hardened Materials',
+		HB_Hardness: '> 650',
+		HRC_Hardness: '> 60',
+		standard_Din: ['-'],
+	}
+}
